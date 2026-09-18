58021d62523b0556bbc34963772062bbbfc1a199
sujay nayek <sujaynayek7384@gmail.com>
2026-09-18 19:51:17 +0530

    add backend server to frontend

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5173",
+      "https://nuzio-ai-jhmp.onrender.com"
     ],
     credentials: true,
   }),
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api" || "https://nuzio-ai-jhmp.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
